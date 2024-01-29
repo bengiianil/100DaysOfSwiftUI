62778be797383728660d7017d4f42a127c3c9b29
Bengi Anıl <58110156+bengiianil@users.noreply.github.com>
2024-01-29 22:28:49 +0300

    Added accessibility sandbox project

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Bengi Anıl on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
