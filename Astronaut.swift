a25b966d1c00b313f863508455c611a36156575a
Bengi Anıl <58110156+bengiianil@users.noreply.github.com>
2024-01-29 21:14:57 +0300

    Finished moonshot project

---
+++ Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Bengi Anıl on 15/11/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
